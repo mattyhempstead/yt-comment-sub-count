e41d600db8ddef717dfc2c1df84d1bb5210c38ee
Matty <matty.hempstead@gmail.com>
2020-02-29 00:38:49 +1100

    Remove 's' when commenter only has 1 subscriber

--- src/js/content.js
+++ src/js/content.js
@@ -78,7 +78,7 @@ onChildLoad(document.querySelector('ytd-comments#comments'), 'ytd-item-section-r
 
           const subCounterSpan = document.createElement('span');
           el.querySelector('div#header-author').appendChild(subCounterSpan);
-          subCounterSpan.innerHTML = `${subs} subscriber${subs !== 1 && 's'}`;
+          subCounterSpan.innerHTML = `${subs} subscriber${subs === '1' ? '' : 's'}`;
           subCounterSpan.style.fontSize = '1.1em';
           subCounterSpan.style.color = '#ddd';
           subCounterSpan.style.backgroundColor = '#333';
